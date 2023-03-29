86a372e6bce70fbc9c46cdd33df871e71a6c5ff2
devnishant29 <devnishant29@gmail.com>
2023-03-29 14:13:15 +0530

    error fix

--- server/index.js
+++ server/index.js
@@ -5,72 +5,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-// app.post("/register", (req, res) => {
-//   const email = req.body.email;
-//   const username = req.body.username;
-//   const password = req.body.password;
-
-
-
-
-  // con.connect(function (err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  //   var sql =
-  //     "INSERT INTO MY users (email, username, password) VALUES(email, username, password)";
-  //     console.log(sql);
-  //   con.query(sql, function (err, result) {
-  //     if (result) {
-  //       res.send(result);
-  //     } else {
-  //       res.send({ message: "Enter correct asked details" });
-  //     }
-  //   });
-  // });
-
-  // con.query(
-  //   "INSERT INTO MY users (email, username, password) VALUES(?, ?, ?)",
-  //   { email, username, password },
-  //   (err, result) => {
-  //     console.log(result);
-  //     if (result) {
-  //       res.send(result);
-  //     } else {
-  //       res.send({ message: "Enter correct asked details" });
-  //     }
-  //   }
-  // );
-// });
-
-
-
-// app.listen(3001, () => {
-//   console.log("running backend server");
-// });
-
-
-var mysql=require("mysql")
-var con=mysql.createConnection({
-  host: 'localhost',
+var mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "localhost",
   user: "root",
   password: "30082003",
-  database: "register"
+  database: "register",
 });
 
-con.connect(function(error) {
-  if(error) throw error;
-  console.log("connected")
-})
+con.connect(function (error) {
+  if (error) throw error;
+  console.log("connected");
+});
 
 app.post("/login", (req, res) => {
+  
   const username = req.body.username;
   const password = req.body.password;
 
   con.query(
-    "SELECT * FROM users WHERE username = ? AND password = ?",
-    { username, password },
-    (err, result) => {
+    "select * from users where username = ? and userpassword = ?",
+    [username, password],
+    function (err, result) {
+      console.log(result);
       if (err) {
         req.setEncoding({ err: err });
       } else {
@@ -84,7 +41,6 @@ app.post("/login", (req, res) => {
   );
 });
 
-
 app.post("/register", (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
@@ -92,22 +48,18 @@ app.post("/register", (req, res) => {
   console.log(username);
   console.log(email);
   console.log(password);
-
-  con.query(
-      "INSERT INTO  users (email, username, password) VALUES(?, ?, ?)", {email, username, password},
-      (err, result) => {
-        console.log(result);
-        if (result) {
-          res.send(result);
-        } else {
-          res.send({ message: "Enter correct asked details" });
-        }
-      }
-    );
+  var sql = "INSERT INTO  users (email, username, password) VALUES ?";
+  var values = [[email, username, password]];
+  con.query(sql, [values], function (err, result) {
+    console.log(result);
+    if (result) {
+      res.send(result);
+    } else {
+      res.send({ message: "Enter correct asked details" });
+    }
+  });
 });
-
 
 app.listen(3001, () => {
   console.log("running backend server");
 });
-
